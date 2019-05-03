d90037e20fed0881349f9fd74996f96fc7703f83
nobuakira <50226058+nobuakira@users.noreply.github.com>
2019-05-03 15:55:27 +0900

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'a)u2=hy&kia+pk**b@7j)g#f3z=%=slq!!s&q7^(t^eet7(_p^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','pythonanywhare.com']
+ALLOWED_HOSTS = ['127.0.0.1','pythonanywhere.com']
 
 
 # Application definition
